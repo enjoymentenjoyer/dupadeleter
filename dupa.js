fb16f2c4128a730adc508db946200fbda3fe9e5f
Enjoyer <enjoymentenjoyer@protonmail.com>
2021-10-30 10:00:13 +0200

    Query the page on scroll and hide unenjoyable comments

---
+++ dupa.js
@@ -0,0 +1,7 @@
+// poop
+window.addEventListener('scroll', () => {
+    // begone
+    document.querySelectorAll(
+        'ytd-comment-thread-renderer > ytd-comment-renderer div#author-thumbnail a[href="/channel/UC3RIcfnBQinhnhKHCIEA16Q"]'
+    ).forEach(dupa => dupa.parentNode.parentNode.parentNode.parentNode.hidden = true);
+});
